ecf06d8296b0b110490e693422eeafc34ac432a6
Ole Cordsen <ole.cordsen@cap3.de>
2018-02-02 14:21:29 +0100

    add navigation items.

--- components/app/app.tsx
+++ components/app/app.tsx
@@ -3,15 +3,39 @@ import {HeaderComponent} from "../header/header";
 import {SidebarComponent} from "../sidebar/sidebar";
 import {ContentComponent} from "../content/content";
 
-export class AppComponent extends React.Component {
+interface Props {
+
+}
+
+interface State {
+    readonly selectedNavigationItem: string;
+}
+
+export class AppComponent extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            selectedNavigationItem: ""
+        };
+    }
+
     render() {
+        const {selectedNavigationItem} = this.state;
+
+        const selectNavigationItem = (navigationItem: string) => {
+            this.setState({
+                selectedNavigationItem: navigationItem
+            });
+        };
+
         return (
             <div className="app-component">
                 <HeaderComponent/>
 
                 <div className="container">
-                    <SidebarComponent/>
-                    <ContentComponent/>
+                    <SidebarComponent selectNavigationItem={selectNavigationItem}/>
+                    <ContentComponent selectedNavigationItem={selectedNavigationItem}/>
                 </div>
             </div>
         );
--- components/content/content.tsx
+++ components/content/content.tsx
@@ -1,7 +1,24 @@
 import * as React from "react";
 
-export class ContentComponent extends React.Component {
+interface Props {
+    readonly selectedNavigationItem: string;
+}
+
+export class ContentComponent extends React.Component<Props> {
     render() {
-        return <div className="content-component"></div>;
+        return (
+            <div className="content-component">
+                {this.renderContent(this.props.selectedNavigationItem)}
+            </div>
+        );
     }
+
+    private renderContent = (navigationItem: string) => {
+        switch (navigationItem) {
+            case "Übersicht":
+                return <div>Übersicht</div>;
+            default:
+                return <div>Alles andere</div>;
+        }
+    };
 }
