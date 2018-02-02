f0ef3d578920e6bf85dd12bd1607677ea7d0f4eb
Ole Cordsen <ole.cordsen@cap3.de>
2018-02-02 11:26:33 +0100

    add content component.

--- components/app.tsx
+++ components/app.tsx
@@ -1,7 +1,16 @@
 import * as React from "react";
+import {HeaderComponent} from "./header";
+import {SidebarComponent} from "./sidebar";
+import {ContentComponent} from "./content";
 
 export class AppComponent extends React.Component {
     render() {
-        return <div className="app-component">App</div>;
+        return (
+            <div className="app-component">
+                <HeaderComponent/>
+                <SidebarComponent/>
+                <ContentComponent/>
+            </div>
+        );
     }
 }
