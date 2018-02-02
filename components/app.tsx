3d440ee2f60114c569678b4ef0cd245aae9bde81
Kai Oppermann <kai.oppermann@esn.de>
2018-02-02 11:24:10 +0100

    Umbau

--- components/app.tsx
+++ components/app.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import {HeaderComponent} from "./header";
+import {SidebarComponent} from "./sidebar";
 
 export class AppComponent extends React.Component {
     render() {
-        return <div className="app-component">App</div>;
+        return (
+            <div className="app-component">
+                <HeaderComponent/>
+                <SidebarComponent/>
+            </div>
+        );
     }
 }
